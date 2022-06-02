7d9d9dd3d1b562dfbc75c13a457361613f1d46b4
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-06-02 15:58:14 +0530

    a=input() for i in range(len(a)-1,-1,-1):     if(a[i]==' '):         for j in range(i+1,len(a)):             if(a[j]==' '):                 break             print(a[j],end='')         print(' ',end='') for i in a:     if(i!=' '):         print(i,end='')     else:         break

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,2 +1,13 @@
 a=input()
-print(a[::-1])
+for i in range(len(a)-1,-1,-1):
+    if(a[i]==' '):
+        for j in range(i+1,len(a)):
+            if(a[j]==' '):
+                break
+            print(a[j],end='')
+        print(' ',end='')
+for i in a:
+    if(i!=' '):
+        print(i,end='')
+    else:
+        break
