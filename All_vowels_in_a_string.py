a0bd3430c9316b77eb6ba4cd8bc39fafbf6a2eff
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-06-29 19:49:00 +0530

    a=input() l=[0,0,0,0,0] b='aeiou' n=0 for i in a:     if i in 'aeiou':         if i=='a':             l[n]+=1         elif i=='e':             l[n+1]+=1         elif i=='i':             l[n+2]+=1         elif i=='o':             l[n+3]+=1         else:             l[n+4]+=1 count=0 for i in range(0,5):     if l[i]==0:         count=1 if count==0:     print('True') else:     print('False')

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,15 +1,23 @@
-n=input()
-b=[]
-d=[]
-a='aeiou'
-count=0
-for i in n:
-    if i in 'aeiou' and i not in b:
-        b.append(i)
-c=''.join(b)
+a=input()
+l=[0,0,0,0,0]
+b='aeiou'
+n=0
 for i in a:
-    if i not in c:
-        count+=1
+    if i in 'aeiou':
+        if i=='a':
+            l[n]+=1
+        elif i=='e':
+            l[n+1]+=1
+        elif i=='i':
+            l[n+2]+=1
+        elif i=='o':
+            l[n+3]+=1
+        else:
+            l[n+4]+=1
+count=0
+for i in range(0,5):
+    if l[i]==0:
+        count=1
 if count==0:
     print('True')
 else:
