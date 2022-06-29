33659edb9101d7f31b90377e2f46f86fb7438180
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-06-29 19:46:58 +0530

    a=input() l=[0,0,0,0,0] b='aeiou' n=0 for i in a:     if i in 'aeiou':         if i=='a':             l[n]+=1         elif i=='e':             l[n+1]+=1         elif i=='i':             l[n+2]+=1         elif i=='o':             l[n+3]+=1         else:             l[n+4]+=1 count=0 for i in range(0,5):     if l[i]==0:         for j in range(0,5):             if i==j:                 count+=1                 print(b[i],end=' ') if count==0:     print('0')

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,18 +1,25 @@
-n=input()
-b=[]
-d=[]
-a='aeiou'
-count=0
-for i in n:
-    if i in 'aeiouAEIOU' and i not in b:
-        b.append(i)
-c=''.join(b)
+a=input()
+l=[0,0,0,0,0]
+b='aeiou'
+n=0
 for i in a:
-    if i not in c:
-        d.append(i)
-        count+=1
+    if i in 'aeiou':
+        if i=='a':
+            l[n]+=1
+        elif i=='e':
+            l[n+1]+=1
+        elif i=='i':
+            l[n+2]+=1
+        elif i=='o':
+            l[n+3]+=1
+        else:
+            l[n+4]+=1
+count=0
+for i in range(0,5):
+    if l[i]==0:
+        for j in range(0,5):
+            if i==j:
+                count+=1
+                print(b[i],end=' ')
 if count==0:
     print('0')
-else:
-    e=''.join(d)
-    print(*e)
