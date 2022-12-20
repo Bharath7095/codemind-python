c5aa194ae581a2ce336ba0985d57a6d09019940a
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-12-20 12:27:39 +0530

    def palin(n):     a=n     rev=0     while(a!=0):         r=a%10         a//=10         rev=rev*10+r     if(rev==n):         return 1     return 0 num=int(input()) a=num b=num while(1):     a+=1     if(palin(a)):         break while(1):     b-=1     if(palin(b)):         break if(a-num<num-b):     print(a) elif(a-num>num-b):     print(b) else:     print(b,a)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,24 +1,27 @@
 def palin(n):
+    a=n
     rev=0
-    x=n
-    while x:
-        r=x%10
-        rev=(rev*10)+r
-        x//=10
-    if rev==n:
+    while(a!=0):
+        r=a%10
+        a//=10
+        rev=rev*10+r
+    if(rev==n):
         return 1
     return 0
-n=int(input())
-d=1
-a=0
-while True:
-    if(palin(n-d)):
-        a=1
-        print(n-d,end=' ')
-    if(palin(n+d)):
-        a=1
-        print(n+d)
-    if a==1:
+num=int(input())
+a=num
+b=num
+while(1):
+    a+=1
+    if(palin(a)):
         break
-    else:
-        d+=1
+while(1):
+    b-=1
+    if(palin(b)):
+        break
+if(a-num<num-b):
+    print(a)
+elif(a-num>num-b):
+    print(b)
+else:
+    print(b,a)
