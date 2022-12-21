7ae13ca132c02d97cfec4b8a47f35a3ddf4e075e
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-12-21 21:31:24 +0530

    a=input() a=a.split(" ") c=99999 for i in a:     l=len(i)     if(l<c):         c=l print(c)

--- minimum_length_of_words.py
+++ minimum_length_of_words.py
@@ -1,7 +1,8 @@
 a=input()
-b=list(a.split( ))
-l=50
-for i in b:
-    if len(i)<l:
-        l=len(i)
-print(l)
+a=a.split(" ")
+c=99999
+for i in a:
+    l=len(i)
+    if(l<c):
+        c=l
+print(c)
